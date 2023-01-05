c33b6c9e548879ae8548053d98e1ecfd526ced4c
SPARTACUS5329 <adityaspartacus5329@gmail.com>
2023-01-05 18:52:15 +0530

    screenshotting measured canvas

--- src/App.js
+++ src/App.js
@@ -7,316 +7,361 @@ import PreviewPopup, { Face } from "./components/PreviewPopup";
 import { Button, Modal } from "@mui/material";
 
 function App() {
-  const viewer = useRef(null);
-  const [textures, setTextures] = useState([]);
-  const [approved, setApproved] = useState(0);
+	const viewer = useRef(null);
+	const [textures, setTextures] = useState([]);
+	const [approved, setApproved] = useState(0);
 
-  const [openSnippet, setOpenSnippet] = useState(false);
-  const handleOpenSnippet = () => setOpenSnippet(true);
-  const handleCloseSnippet = () => setOpenSnippet(false);
+	const [openSnippet, setOpenSnippet] = useState(false);
+	const handleOpenSnippet = () => setOpenSnippet(true);
+	const handleCloseSnippet = () => setOpenSnippet(false);
 
-  const removeTexture = (index) => {
-    const copyOfT = [...textures];
-    setApproved(approved - 1);
-    setTextures(copyOfT.splice(index));
-  };
-  useEffect(() => {
-    // const downloadURI = (uri, name) => {
-    //   const link = document.createElement("a");
-    //   link.download = name;
-    //   link.href = uri;
-    //   document.body.appendChild(link);
-    //   link.click();
-    //   document.body.removeChild(link);
-    // };
-    WebViewer(
-      {
-        path: "/webviewer/lib",
-        initialDoc:"/assets/pdftron_using_guide.pdf",
-        enableFilePicker: true, // enable FILE SYSTEM
-      },
-      viewer.current
-    ).then((instance) => {
-      // sniping tool code
-      const { docViewer, Annotations, Tools, iframeWindow, annotManager } =
-        instance;
+	const removeTexture = (index) => {
+		const copyOfT = [...textures];
+		setApproved(approved - 1);
+		setTextures(copyOfT.splice(index));
+	};
+	useEffect(() => {
+		WebViewer(
+			{
+				path: "/webviewer/lib",
+				initialDoc: "/assets/test.pdf",
+				enableFilePicker: true, // enable FILE SYSTEM
+				enableMeasurement: true,
+				fullAPI: true,
+			},
+			viewer.current
+		).then((instance) => {
+			// sniping tool code
+			const { docViewer, Annotations, Tools, iframeWindow, annotManager } = instance;
 
-      const createSnipTool = function () {
-        const SnipTool = function () {
-          Tools.RectangleCreateTool.apply(this, arguments);
-          this.defaults.StrokeColor = new Annotations.Color("#F69A00");
-          this.defaults.StrokeThickness = 2;
-        };
+			const { documentViewer, annotationManager } = instance.Core;
 
-        SnipTool.prototype = new Tools.RectangleCreateTool();
+			documentViewer.addEventListener("documentLoaded", async () => {
+				const measurementTool = instance.Core.documentViewer.getTool(
+					"AnnotationCreateDistanceMeasurement"
+				);
 
-        return new SnipTool(docViewer);
-      };
+				measurementTool.setSnapMode(instance.Core.Tools.SnapModes.DEFAULT);
+				instance.Core.annotationManager.setSnapDefaultOptions({
+					indicatorColor: "#00a5e4",
+				});
+			});
 
-      const customSnipTool = createSnipTool();
+			annotationManager.addEventListener("annotationChanged", (annotations, action) => {
+				if (action === "add") {
+					// An annotation is an measurement annotation if it contains a Measure property
+					const measurementAnnotations = annotations.filter(
+						(annotation) => annotation.Measure
+					);
 
-      instance.registerTool({
-        toolName: "SnipTool",
-        toolObject: customSnipTool,
-        buttonImage: "/cut-solid.svg",
-        buttonName: "snipToolButton",
-        tooltip: "Snipping Tool",
-      });
+					measurementAnnotations.forEach((annotation) => {
+						console.log(annotation);
+						const pageNumber = annotation.PageNumber;
+						// get the canvas for the page
+						const pageContainer = iframeWindow.document.getElementById(
+							"pageContainer" + pageNumber
+						);
+						const pageCanvas = pageContainer.querySelector(".canvas" + pageNumber);
 
-      instance.setHeaderItems((header) => {
-        header.push({
-          type: "toolButton",
-          toolName: "SnipTool",
-        });
-      });
+						const scale = window.devicePixelRatio;
+						const topOffset = (parseFloat(pageCanvas.style.top) || 0) * scale;
+						const leftOffset = (parseFloat(pageCanvas.style.left) || 0) * scale;
+						console.log(topOffset, leftOffset);
+						const zoom = docViewer.getZoomLevel() * scale;
 
-      customSnipTool.on("annotationAdded", (annotation) => {
-        const pageNumber = annotation.PageNumber;
-        // get the canvas for the page
-        const pageContainer = iframeWindow.document.getElementById(
-          "pageContainer" + pageNumber
-        );
-        const pageCanvas = pageContainer.querySelector(".canvas" + pageNumber);
+						const x = annotation.X * zoom;
+						const y = annotation.Y * zoom;
+						const width = annotation.Width * zoom;
+						const height = annotation.Height * zoom;
 
-        const scale = window.devicePixelRatio;
-        const topOffset = (parseFloat(pageCanvas.style.top) || 0) * scale;
-        const leftOffset = (parseFloat(pageCanvas.style.left) || 0) * scale;
-        const zoom = docViewer.getZoomLevel() * scale;
+						const copyCanvas = document.createElement("canvas");
+						copyCanvas.width = width;
+						copyCanvas.height = height;
+						const ctx = copyCanvas.getContext("2d");
 
-        const x = annotation.X * zoom - leftOffset;
-        const y = annotation.Y * zoom - topOffset;
-        const width = annotation.Width * zoom;
-        const height = annotation.Height * zoom;
+						ctx.drawImage(pageCanvas, x, y, width, height, 0, 0, width, height);
 
-        const copyCanvas = document.createElement("canvas");
-        copyCanvas.width = width;
-        copyCanvas.height = height;
-        const ctx = copyCanvas.getContext("2d");
+						const imageURL = copyCanvas.toDataURL();
 
-        // copy the image data from the page to a new canvas so we can get the data URL
+						setTextures((pre) => [...pre, imageURL]);
+						handleOpenSnippet();
 
-        ctx.drawImage(pageCanvas, x, y, width, height, 0, 0, width, height);
+						annotManager.deleteAnnotation(annotation);
+					});
+				}
+			});
 
-        const imageURL = copyCanvas.toDataURL();
+			const createSnipTool = function () {
+				const SnipTool = function () {
+					Tools.RectangleCreateTool.apply(this, arguments);
+					this.defaults.StrokeColor = new Annotations.Color("#F69A00");
+					this.defaults.StrokeThickness = 2;
+				};
 
-        // console.log(widthList, heightList);
+				SnipTool.prototype = new Tools.RectangleCreateTool();
 
-        //popup to check the image url, wont be rendered yet
+				return new SnipTool(docViewer);
+			};
 
-        //add to texture state if the snippet is correct
-        setTextures((pre) => [...pre, imageURL]);
-        handleOpenSnippet();
+			const customSnipTool = createSnipTool();
 
-        // downloadURI(copyCanvas.toDataURL(), "sample.jpeg");
+			instance.registerTool({
+				toolName: "SnipTool",
+				toolObject: customSnipTool,
+				buttonImage: "/cut-solid.svg",
+				buttonName: "snipToolButton",
+				tooltip: "Snipping Tool",
+			});
 
-        annotManager.deleteAnnotation(annotation);
-      });
-    });
-  }, []);
+			instance.setHeaderItems((header) => {
+				header.push({
+					type: "toolButton",
+					toolName: "SnipTool",
+				});
+			});
 
-  const exportModel = async (textures) => {
-    function exportGLTF(input) {
-      const gltfExporter = new GLTFExporter();
+			customSnipTool.on("annotationAdded", (annotation) => {
+				const pageNumber = annotation.PageNumber;
+				// get the canvas for the page
+				const pageContainer = iframeWindow.document.getElementById(
+					"pageContainer" + pageNumber
+				);
+				const pageCanvas = pageContainer.querySelector(".canvas" + pageNumber);
 
-      const options = {
-        trs: params.trs,
-        onlyVisible: params.onlyVisible,
-        truncateDrawRange: params.truncateDrawRange,
-        binary: params.binary,
-        maxTextureSize: params.maxTextureSize,
-      };
-      gltfExporter.parse(
-        input,
-        function (result) {
-          if (result instanceof ArrayBuffer) {
-            saveArrayBuffer(result, "scene.glb");
-          } else {
-            const output = JSON.stringify(result, null, 2);
-            console.log(output);
-            saveString(output, "scene.gltf");
-          }
-        },
-        function (error) {
-          console.log("An error happened during parsing", error);
-        },
-        options
-      );
-    }
+				const scale = window.devicePixelRatio;
+				const topOffset = (parseFloat(pageCanvas.style.top) || 0) * scale;
+				const leftOffset = (parseFloat(pageCanvas.style.left) || 0) * scale;
+				const zoom = docViewer.getZoomLevel() * scale;
 
-    const link = document.createElement("a");
-    link.style.display = "none";
-    document.body.appendChild(link); // Firefox workaround, see #6594
+				const x = annotation.X * zoom - leftOffset;
+				const y = annotation.Y * zoom - topOffset;
+				const width = annotation.Width * zoom;
+				const height = annotation.Height * zoom;
 
-    function save(blob, filename) {
-      link.href = URL.createObjectURL(blob);
-      link.download = filename;
-      link.click();
-      // URL.revokeObjectURL( url ); breaks Firefox...
-    }
+				const copyCanvas = document.createElement("canvas");
+				copyCanvas.width = width;
+				copyCanvas.height = height;
+				const ctx = copyCanvas.getContext("2d");
 
-    function saveString(text, filename) {
-      save(new Blob([text], { type: "text/plain" }), filename);
-    }
+				// copy the image data from the page to a new canvas so we can get the data URL
 
-    function saveArrayBuffer(buffer, filename) {
-      save(new Blob([buffer], { type: "application/octet-stream" }), filename);
-    }
+				ctx.drawImage(pageCanvas, x, y, width, height, 0, 0, width, height);
 
-    let camera, object, scene1;
+				const imageURL = copyCanvas.toDataURL();
 
-    const params = {
-      trs: false,
-      onlyVisible: true,
-      truncateDrawRange: true,
-      binary: false,
-      maxTextureSize: 4096,
-    };
+				// console.log(widthList, heightList);
 
-    init();
+				//popup to check the image url, wont be rendered yet
 
-    async function init() {
-      scene1 = new THREE.Scene();
-      scene1.name = "Scene1";
+				//add to texture state if the snippet is correct
+				setTextures((pre) => [...pre, imageURL]);
+				handleOpenSnippet();
 
-      // Perspective Camera
-      camera = new THREE.PerspectiveCamera(
-        45,
-        window.innerWidth / window.innerHeight,
-        1,
-        2000
-      );
-      camera.position.set(600, 400, 0);
-      camera.name = "PerspectiveCamera";
-      scene1.add(camera);
+				// downloadURI(copyCanvas.toDataURL(), "sample.jpeg");
 
-      // Ambient light
-      const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
-      ambientLight.name = "AmbientLight";
-      scene1.add(ambientLight);
+				annotManager.deleteAnnotation(annotation);
+			});
+		});
+	}, []);
 
-      // DirectLight
-      const dirLight = new THREE.DirectionalLight(0xffffff, 1);
-      dirLight.target.position.set(0, 0, -1);
-      dirLight.add(dirLight.target);
-      dirLight.lookAt(-1, -1, 0);
-      dirLight.name = "DirectionalLight";
-      scene1.add(dirLight);
+	const exportModel = async (textures) => {
+		function exportGLTF(input) {
+			const gltfExporter = new GLTFExporter();
 
-      //get deminions
-      let x, y, z;
-      let areaMax = 0;
-      let areaMin = Infinity;
+			const options = {
+				trs: params.trs,
+				onlyVisible: params.onlyVisible,
+				truncateDrawRange: params.truncateDrawRange,
+				binary: params.binary,
+				maxTextureSize: params.maxTextureSize,
+			};
+			gltfExporter.parse(
+				input,
+				function (result) {
+					if (result instanceof ArrayBuffer) {
+						saveArrayBuffer(result, "scene.glb");
+					} else {
+						const output = JSON.stringify(result, null, 2);
+						console.log(output);
+						saveString(output, "scene.gltf");
+					}
+				},
+				function (error) {
+					console.log("An error happened during parsing", error);
+				},
+				options
+			);
+		}
 
-      //loadTextures
-      let loadedTextures = [];
-      const loadTexture = async (i) => {
-        return new Promise((reslove) => {
-          let image = new Image();
-          image.src = textures[i];
-          image.onload = function () {
-            let texture = new THREE.Texture();
-            texture.needsUpdate = true;
-            texture.image = image;
-            const w = parseFloat(this.width);
-            const h = parseFloat(this.height);
-            let tempArea = w * h;
-            if (tempArea > areaMax) {
-              areaMax = tempArea;
-              x = w > h ? w : h;
-              y = w > h ? h : w;
-            }
-            if (tempArea < areaMin) {
-              areaMin = tempArea;
-              z = w > h ? h : w;
-            }
+		const link = document.createElement("a");
+		link.style.display = "none";
+		document.body.appendChild(link); // Firefox workaround, see #6594
 
-            reslove(texture);
-          };
-        });
-      };
+		function save(blob, filename) {
+			link.href = URL.createObjectURL(blob);
+			link.download = filename;
+			link.click();
+			// URL.revokeObjectURL( url ); breaks Firefox...
+		}
 
-      //load all textures as images
-      for (let index = 0; index < 6; index++) {
-        const loadedTexture = await loadTexture(index);
-        loadedTextures.push(loadedTexture);
-      }
-      const textureCubeCustom = loadedTextures.map(
-        (texture) =>
-          new THREE.MeshBasicMaterial({
-            map: texture,
-          })
-      );
+		function saveString(text, filename) {
+			save(new Blob([text], { type: "text/plain" }), filename);
+		}
 
-      //cuboid
-      object = new THREE.Mesh(
-        new THREE.BoxGeometry(x, y, z),
-        textureCubeCustom
-      );
-      object.position.set(0, 0, 0);
-      object.name = "Cube";
-      scene1.add(object);
+		function saveArrayBuffer(buffer, filename) {
+			save(new Blob([buffer], { type: "application/octet-stream" }), filename);
+		}
 
-      // camera
-      const cameraOrtho = new THREE.OrthographicCamera(
-        window.innerWidth / -2,
-        window.innerWidth / 2,
-        window.innerHeight / 2,
-        window.innerHeight / -2,
-        0.1,
-        10
-      );
-      scene1.add(cameraOrtho);
-      cameraOrtho.name = "OrthographicCamera";
+		let camera, object, scene1;
 
-      //export the scene
-      exportGLTF(scene1);
-    }
-  };
+		const params = {
+			trs: false,
+			onlyVisible: true,
+			truncateDrawRange: true,
+			binary: false,
+			maxTextureSize: 4096,
+		};
 
-  useEffect(() => console.log(textures), [textures]);
+		init();
 
-  return (
-    <div className="App">
-      <Modal open={openSnippet} onClose={handleCloseSnippet}>
-        <Face
-          img={textures[textures.length - 1]}
-          remove={() => {
-            setTextures((old) => old.slice(undefined, -2));
-            handleCloseSnippet();
-          }}
-          approve={() => handleCloseSnippet()}
-        />
-      </Modal>
-      <div
-        className="header"
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-        }}
-      >
-        <div>ManageArtworks</div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-          }}
-        >
-          <div>Faces Obtained- {textures.length} / 6</div>{" "}
-          <PreviewPopup
-            textures={textures}
-            removeTexture={removeTexture}
-            approveTexture={null}
-          />
-          <Button variant="contained" onClick={() => exportModel(textures)}>
-            Convert
-          </Button>
-        </div>
-      </div>
-      <div className="webviewer" ref={viewer}></div>
-      {/* <div className="webviewer" ref={viewer}></div> */}
-    </div>
-  );
+		async function init() {
+			scene1 = new THREE.Scene();
+			scene1.name = "Scene1";
+
+			// Perspective Camera
+			camera = new THREE.PerspectiveCamera(
+				45,
+				window.innerWidth / window.innerHeight,
+				1,
+				2000
+			);
+			camera.position.set(600, 400, 0);
+			camera.name = "PerspectiveCamera";
+			scene1.add(camera);
+
+			// Ambient light
+			const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+			ambientLight.name = "AmbientLight";
+			scene1.add(ambientLight);
+
+			// DirectLight
+			const dirLight = new THREE.DirectionalLight(0xffffff, 1);
+			dirLight.target.position.set(0, 0, -1);
+			dirLight.add(dirLight.target);
+			dirLight.lookAt(-1, -1, 0);
+			dirLight.name = "DirectionalLight";
+			scene1.add(dirLight);
+
+			//get deminions
+			let x, y, z;
+			let areaMax = 0;
+			let areaMin = Infinity;
+
+			//loadTextures
+			let loadedTextures = [];
+			const loadTexture = async (i) => {
+				return new Promise((reslove) => {
+					let image = new Image();
+					image.src = textures[i];
+					image.onload = function () {
+						let texture = new THREE.Texture();
+						texture.needsUpdate = true;
+						texture.image = image;
+						const w = parseFloat(this.width);
+						const h = parseFloat(this.height);
+						let tempArea = w * h;
+						if (tempArea > areaMax) {
+							areaMax = tempArea;
+							x = w > h ? w : h;
+							y = w > h ? h : w;
+						}
+						if (tempArea < areaMin) {
+							areaMin = tempArea;
+							z = w > h ? h : w;
+						}
+
+						reslove(texture);
+					};
+				});
+			};
+
+			//load all textures as images
+			for (let index = 0; index < 6; index++) {
+				const loadedTexture = await loadTexture(index);
+				loadedTextures.push(loadedTexture);
+			}
+			const textureCubeCustom = loadedTextures.map(
+				(texture) =>
+					new THREE.MeshBasicMaterial({
+						map: texture,
+					})
+			);
+
+			//cuboid
+			object = new THREE.Mesh(new THREE.BoxGeometry(x, y, z), textureCubeCustom);
+			object.position.set(0, 0, 0);
+			object.name = "Cube";
+			scene1.add(object);
+
+			// camera
+			const cameraOrtho = new THREE.OrthographicCamera(
+				window.innerWidth / -2,
+				window.innerWidth / 2,
+				window.innerHeight / 2,
+				window.innerHeight / -2,
+				0.1,
+				10
+			);
+			scene1.add(cameraOrtho);
+			cameraOrtho.name = "OrthographicCamera";
+
+			//export the scene
+			exportGLTF(scene1);
+		}
+	};
+
+	useEffect(() => console.log(textures), [textures]);
+
+	return (
+		<div className="App">
+			<Modal open={openSnippet} onClose={handleCloseSnippet}>
+				<Face
+					img={textures[textures.length - 1]}
+					remove={() => {
+						setTextures((old) => old.slice(undefined, -2));
+						handleCloseSnippet();
+					}}
+					approve={() => handleCloseSnippet()}
+				/>
+			</Modal>
+			<div
+				className="header"
+				style={{
+					display: "flex",
+					flexDirection: "row",
+					justifyContent: "space-between",
+				}}>
+				<div>ManageArtworks</div>
+				<div
+					style={{
+						display: "flex",
+						flexDirection: "row",
+					}}>
+					<div>Faces Obtained- {textures.length} / 6</div>{" "}
+					<PreviewPopup
+						textures={textures}
+						removeTexture={removeTexture}
+						approveTexture={null}
+					/>
+					<Button variant="contained" onClick={() => exportModel(textures)}>
+						Convert
+					</Button>
+				</div>
+			</div>
+			<div className="webviewer" ref={viewer}></div>
+			{/* <div className="webviewer" ref={viewer}></div> */}
+		</div>
+	);
 }
 
 export default App;
